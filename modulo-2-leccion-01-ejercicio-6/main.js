eb00b2d61e5b4067733471909f419451c6ff7a1b
Maria Ozamiz <ozamiz.m@gmail.com>
2020-06-18 20:10:31 +0200

    create inner content

---
+++ modulo-2-leccion-01-ejercicio-6/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const div = document.querySelector(".box");
+const content =
+    '<h1>Lorem Ipsum</h1><img src="http://via.placeholder.com/350x150"><p>Lorem ipsum dolor sit amet</p>';
+div.innerHTML = content;
