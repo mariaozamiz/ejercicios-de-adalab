3e6c40f053a80a486c3242c00c47babbe5aec6b7
Maria Ozamiz <ozamiz.m@gmail.com>
2020-06-19 10:12:39 +0200

    add opacity

--- modulo-2-leccion-01-ejercicios-7-8/main.js
+++ modulo-2-leccion-01-ejercicios-7-8/main.js
@@ -1,1 +1,7 @@
 "use strict";
+const menu = document.querySelector(".menu");
+const menuContent = "<li>texto1</li><li>texto2</li><li>texto3</li>";
+menu.innerHTML = menuContent;
+
+const button2 = document.querySelector("#button--2");
+button2.classList.add("opacity");
