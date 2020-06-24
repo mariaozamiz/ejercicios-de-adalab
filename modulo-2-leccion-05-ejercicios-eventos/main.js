c2dab426892a7682110a5e8b1e2e88d0f87d1e1b
Maria Ozamiz <ozamiz.m@gmail.com>
2020-06-24 14:49:17 +0200

    add events

---
+++ modulo-2-leccion-05-ejercicios-eventos/main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// EJERCICIO 1 - hola click
+
+// const button = document.querySelector('.button');
+// const paragraph = document.querySelector('.p');
+
+// function showMessage() {
+//     paragraph.innerHTML = 'Mi primer click, ¡ole yo!';
+// }
+// button.addEventListener('click', showMessage);
+
+// EJERCICIO 2 - ¿cómo te llamas?
+
+// const button2 = document.querySelector('.button--2');
+// const textArea = document.querySelector('.input');
+
+// function showGreeting() {
+//     let name = textArea.value;
+//     console.log(`Hola, ${name}`);
+// }
+
+// button2.addEventListener('click', showGreeting);
+
+// EJERCICIO 3 - Dame ipsum
+
+// const paragraph2 = document.querySelector('.p--2');
+
+// function duplicateText() {
+//     const newParagraph = document.createElement('p');
+//     newParagraph.innerHTML = 'Lorem ipsum';
+//     paragraph2.insertAdjacentElement('afterend', newParagraph);
+// }
+
+// paragraph2.addEventListener('mouseover', duplicateText);
+
+// EJERCICIO 4 - Scroll es de colores
+
