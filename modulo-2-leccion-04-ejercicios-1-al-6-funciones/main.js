5ca16a0ca4b27657a9f11538aac647a0891e2332
Maria Ozamiz <ozamiz.m@gmail.com>
2020-06-24 10:16:24 +0200

    add exercises 7 and 10

--- modulo-2-leccion-04-ejercicios-1-al-6-funciones/main.js
+++ modulo-2-leccion-04-ejercicios-1-al-6-funciones/main.js
@@ -35,9 +35,6 @@
 
 // EJERCICIO 4 - pares o nones
 
-// function isItEven(a) {
-//     let result = a % 2 > 0 ? false : true;
-//     return result;
 // }
 // console.log(isItEven(11));
 
@@ -67,3 +64,41 @@
 
 // EJERCICIO 7 - combinando funciones
 
+// function getEl(selector) {
+//     const element = document.querySelector(selector);
+//     return element;
+// }
+
+// function evenOrOdd(a) {
+//     const number = a % 2;
+//     if (number === 0) {
+//         console.log(`El número ${a} es Par`);
+//     } else {
+//         console.log(`El número ${a} es Impar`);
+//     }
+// }
+
+// const text = getEl('.p');
+
+// const value = parseInt(text.innerHTML);
+
+// console.log(value);
+// evenOrOdd(value);
+
+// EJERCICIO 10 - calculador de modelo de caja
+
+function calculateWidth(borderBox, width, padding, border) {
+    const totalPadding = padding * 2;
+    const totalBorder = border * 2;
+
+    if (borderBox === 'border-box') {
+        const result = width + totalPadding + totalBorder;
+        console.log(
+            `El ancho del contenido es: ${width} y el ancho total de la caja es: ${result}`
+        );
+    } else {
+        console.log('No me estás indicando que es border-box');
+    }
+}
+
+calculateWidth('border-nai', 10, 2, 1);
