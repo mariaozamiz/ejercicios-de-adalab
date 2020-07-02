6ee4a17714cdb8677c49a275d7e3ff4b5319a0a6
Maria Ozamiz <ozamiz.m@gmail.com>
2020-07-02 10:21:27 +0200

    attempt on bonus exercises 5 to 7

--- modulo-02-leccion-09-ejercicios-arrays-y-bucles-3/main.js
+++ modulo-02-leccion-09-ejercicios-arrays-y-bucles-3/main.js
@@ -2,3 +2,23 @@
 
 // EJERCICIOS 5, 6 y 7 - Árbol de Navidad
 
+let triangle = '▲';
+let star = '*';
+let trunk = '|';
+let air = '';
+let height;
+let triangles = [];
+// let christmasTree = [];
+
+function growATree(height) {
+    console.log(star);
+    for (let i = 0; i < height; i++) {
+        triangles.push(triangle);
+        console.log(triangles[(0, height)]);
+    }
+    console.log(trunk);
+}
+growATree(5);
+
+christmasTree[0] = star;
+console.log(christmasTree[0]);
