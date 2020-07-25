2b7e3ed79382409ff97da3f1fbdc575a55e29ce6
Maria Ozamiz <ozamiz.m@gmail.com>
2020-07-25 13:36:24 +0200

    add exercises 1, 2 and 4

---
+++ modulo-3-leccion-04-Componentes-en-React/ejercicio-04/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ItemList from './ItemList.js';
+import '../stylesheets/app.css';
+
+function App() {
+    return (
+        <div className="App">
+            <ItemList />
+        </div>
+    );
+}
+
+export default App;
